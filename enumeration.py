b8e01c4c48f618bb4db51df4d04775cdf0af36fe
1n0xesec <105924910+1n0xe@users.noreply.github.com>
2023-08-28 21:55:59 +0200

    Modifs

--- enumeration.py
+++ enumeration.py
@@ -2,54 +2,35 @@
 
 def gather_information():
     print("Collecte d'informations pendant l'énumération...\n")
-
-    # Étape 1 : Informations WHOIS
-    print("1. Informations WHOIS")
-    print("   Utilisez la commande 'whois' ou des outils en ligne pour récupérer les détails d'enregistrement de domaine.")
-    print("   Exemple : whois exemple.com")
-
-    # Étape 2 : Énumération DNS
-    print("2. Énumération DNS")
+    text1 = "1. Énumération DNS"
+    formatted_text1 = "\033[1m\033[4m" + text1 + "\033[0m"
+    # Étape 1 : Énumération DNS
+    print(formatted_text1)
     print("   Utilisez des outils comme 'dnsenum', 'dnsrecon' ou 'dig' pour rassembler des informations liées au DNS.")
-    print("   Exemple : dnsenum exemple.com")
-
-    # Étape 3 : Scan de réseau
-    print("3. Scan de réseau")
+    text2 = "2. Scan de réseau"
+    formatted_text2 = "\033[1m\033[4m" + text2 + "\033[0m"
+    # Étape 2 : Scan de réseau
+    print(formatted_text2)
     print("   Utilisez des outils comme 'nmap' pour repérer les hôtes actifs, les ports ouverts et les services en cours d'exécution.")
-    print("   Exemple : nmap -sV -p 1-65535 exemple.com")
-
-    # Étape 4 : Énumération de sous-domaines
-    print("4. Énumération de sous-domaines")
+    text3 = "3. Énumération de sous-domaines"
+    formatted_text3 = "\033[1m\033[4m" + text3 + "\033[0m"
+    # Étape 3 : Énumération de sous-domaines
+    print(formatted_text3)
     print("   Utilisez des outils comme 'Sublist3r', 'Amass' ou 'Subfinder' pour découvrir des sous-domaines.")
-    print("   Exemple : sublist3r -d exemple.com")
-
-    # Étape 5 : Scraping Web
-    print("5. Scraping Web")
+    text4 = "4. Scraping Web"
+    formatted_text4 = "\033[1m\033[4m" + text4 + "\033[0m"
+    # Étape 4 : Scraping Web
+    print(formatted_text4)
     print("   Utilisez des outils comme 'Wget' ou 'Curl' pour récupérer le contenu web et identifier les technologies utilisées.")
-    print("   Exemple : wget -r -l 2 http://exemple.com")
-
-    # Étape 6 : Réseaux Sociaux
-    print("6. Réseaux Sociaux")
-    print("   Vérifiez les plateformes de réseaux sociaux pour obtenir des informations publiques sur la cible.")
-    print("   Recherchez les profils des employés, les annonces de l'entreprise, etc.")
-
-    # Étape 7 : Google Dorking
-    print("7. Google Dorking")
+    text5 = "5. Google Dorking"
+    formatted_text5 = "\033[1m\033[4m" + text5 + "\033[0m"
+    # Étape 5 : Google Dorking
+    print(formatted_text5)
     print("   Utilisez des requêtes de recherche spécifiques sur Google (dorks) pour trouver des informations potentiellement sensibles.")
-    print("   Exemple : site:exemple.com filetype:pdf")
-
-    # Étape 8 : Recherche Shodan
-    print("8. Recherche Shodan")
-    print("   Utilisez Shodan pour rechercher des appareils connectés à Internet et leurs vulnérabilités.")
-    print("   Exemple : shodan search exemple.com")
-
-    # Étape 9 : Recherche Pastebin
-    print("9. Recherche Pastebin")
-    print("   Recherchez Pastebin pour des informations potentiellement divulguées liées à la cible.")
-    print("   Exemple : site:pastebin.com exemple.com")
-
-    # Étape 10 : Outils OSINT
-    print("10. Outils OSINT")
+    text6 = "6. Outils OSINT"
+    formatted_text6 = "\033[1m\033[4m" + text6 + "\033[0m"
+    # Étape 6 : Outils OSINT
+    print(formatted_text6)
     print("Utilisez des frameworks OSINT tels que 'Maltego', 'theHarvester' et 'SpiderFoot' pour l'automatisation de la collecte d'informations.\n")
     
     print("PENSEZ A PRENDRE DES NOTES (utilisez des outils comme Obsidian)\n")
@@ -57,32 +38,33 @@ def gather_information():
     
 
 def identify_services():
-    print("Identification des services pendant l'énumération...")
-
+    print("Identification des services pendant l'énumération...\n")
+    text7 = "1. Scanning des ports"
+    formatted_text7 = "\033[1m\033[4m" + text7 + "\033[0m"
     # Étape 1 : Scanning des ports
-    print("1. Scanning des ports")
+    print(formatted_text7)
     print("   Utilisez 'nmap' ou d'autres outils similaires pour scanner les ports ouverts.")
-    print("   Exemple : nmap -p 1-65535 exemple.com")
-
+    text8 = "2. Identification des services"
+    formatted_text8 = "\033[1m\033[4m" + text8 + "\033[0m"
     # Étape 2 : Identification des services
-    print("2. Identification des services")
+    print(formatted_text8)
     print("   Analysez les résultats du scan pour identifier les services en cours d'exécution.")
     print("   Utilisez des outils comme 'nmap', 'Netcat' (nc), ou des scripts personnalisés.")
-    print("   Exemple : nmap -sV -p 80,443 exemple.com")
-
+    text9 = "3. Analyse des versions"
+    formatted_text9 = "\033[1m\033[4m" + text9 + "\033[0m"
     # Étape 3 : Analyse des versions
-    print("3. Analyse des versions")
-    print("   Utilisez les informations fournies par les outils pour déterminer les versions des services.")
+    print(formatted_text9)
+    print("   Utilisez les informations fournies par les outils comme nmap pour déterminer les versions des services.")
     print("   Cela peut vous aider à trouver des vulnérabilités connues.")
-    print("   Exemple : nmap -sV -p 80,443 exemple.com")
-
+    text10 = "4. Documentation"
+    formatted_text10 = "\033[1m\033[4m" + text10 + "\033[0m"
     # Étape 4 : Documentation
-    print("4. Documentation")
+    print(formatted_text10)
     print("   Documentez les services identifiés et leurs versions pour référence future.")
-    print("   Cela aidera à planifier la phase d'exploitation et à fournir des rapports.")
+    print("   Cela aidera à planifier la phase d'exploitation et à fournir des rapports.\n")
 
     print("Identification des services terminée.")
-    input("Appuyez sur Entrée pour continuer...")  # Attendre l'entrée de l'utilisateur
+    input("Appuyez sur Entrée pour continuer...")
 
 def run_enumeration():
     print("L'énumération commence")
